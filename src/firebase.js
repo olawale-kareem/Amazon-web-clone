f32e50b8b245ce7713f054b97dfc93007c84d0d0
olawale-kareem <olawale.kareem@decagon.dev>
2021-11-30 18:59:43 +0100

    feature: header implementation

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDe9YAWoJEq6aE776QrVkaKot-nbV4l6Ik",
+    authDomain: "web-clone-509cc.firebaseapp.com",
+    projectId: "web-clone-509cc",
+    storageBucket: "web-clone-509cc.appspot.com",
+    messagingSenderId: "630986255875",
+    appId: "1:630986255875:web:a60cea09b7feb1e4722836",
+    measurementId: "G-65JRG5D1H0"
+  };
